d703cbefe2014fab9d3fb08d1356111ee213ab27
Pozitronik <antikillerxxl@gmail.com>
2021-06-11 11:15:09 +0300

    ErrorHandler (should be used instead od LoggedException)

---
+++ src/models/ErrorHandler.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+
+namespace pozitronik\sys_exceptions\models;
+
+use yii\web\ErrorHandler as YiiErrorHandler;
+
+/**
+ * Class ErrorHandler
+ */
+class ErrorHandler extends YiiErrorHandler {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function logException($exception) {
+		SysExceptions::log($exception);
+	}
+}
