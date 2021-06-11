3b572f7bcc6da0b60008aad56f4f4d1c31861e70
Pozitronik <antikillerxxl@gmail.com>
2021-06-11 11:48:51 +0300

    layout updated (bs4 supported)

--- src/views/index/view.php
+++ src/views/index/view.php
@@ -8,47 +8,45 @@ declare(strict_types = 1);
  */
 
 use pozitronik\sys_exceptions\models\SysExceptions;
-use yii\helpers\Html;
+use pozitronik\sys_exceptions\SysExceptionsModule;
 use yii\web\View;
 use yii\widgets\DetailView;
 
 $this->title = $model->file;
 
 ?>
-<div class="row">
-    <div class="col-xs-12">
-        <div class="panel">
-            <div class="panel-heading">
-                    <div class="panel-control">
-                        <div class="btn-group">
-                            <?= Html::a('Acknowledge', ['acknowledge'], ['class' => 'btn btn-success']) ?>
-                        </div>
-                    </div>
-                <h3 class="panel-title"><?= $this->title ?></h3>
-            </div>
-            <div class="panel-body">
-                <?= DetailView::widget([
-                    'model' => $model,
-                    'attributes' => [
-                        'id',
-                        'timestamp',
-                        [
-                            'attribute' => 'user_id',
-                            'value' => $model->user_id,
-                            'format' => 'raw'
-                        ],
-                        'code',
-                        'file',
-                        'line',
-                        'message',
-                        'trace:ntext',
-                        'get',
-                        'post',
-                        'known:boolean',
-                        'user_agent'
-                    ]
-                ]) ?>
-            </div>
-        </div>
-    </div>
+<div class="panel">
+	<div class="panel-heading panel-hdr">
+		<h2 class="panel-title"><?= $this->title ?></h2>
+		<div class="panel-control panel-toolbar">
+			<?= SysExceptionsModule::a('Acknowledge', ['index/acknowledge'], ['class' => 'btn btn-success ']) ?>
+		</div>
+
+	</div>
+	<div class="panel-body panel-container show">
+		<div class="panel-content">
+			<?= DetailView::widget([
+				'model' => $model,
+				'attributes' => [
+					'id',
+					'timestamp',
+					[
+						'attribute' => 'user_id',
+						'value' => $model->user_id,
+						'format' => 'raw'
+					],
+					'code',
+					'statusCode',
+					'file',
+					'line',
+					'message',
+					'trace:ntext',
+					'get',
+					'post',
+					'known:boolean',
+					'user_agent'
+				]
+			]) ?>
+		</div>
+	</div>
 </div>
