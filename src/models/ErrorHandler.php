fff58340ec7283cc82676be6e973f997a2b03a3f
Pozitronik <antikillerxxl@gmail.com>
2021-06-11 11:56:06 +0300

    parent call

--- src/models/ErrorHandler.php
+++ src/models/ErrorHandler.php
@@ -14,6 +14,7 @@ class ErrorHandler extends YiiErrorHandler {
 	 * @inheritDoc
 	 */
 	public function logException($exception) {
+		parent::logException($exception);
 		SysExceptions::log($exception);
 	}
 }
